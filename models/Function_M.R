9e4f0c3de2e328c4681fd1878fb0d13ad4b9fe13
lilydun <l.dun@student.unsw.edu.au>
2021-08-26 11:07:51 +1000

    plot

--- models/Function_M.R
+++ models/Function_M.R
@@ -106,6 +106,28 @@ L = data_f %>%
 
 I_z = I_0*exp(-k*L)
 
+outputs <- data.frame(absPAR = I_z, H = 15, V = 0.5, L = 4.402, F = 1.99, S = 1)
+
+outputs %>% 
+  ggplot(aes(F, absPAR)) + 
+  geom_point(aes(colour = as.factor(H))) + 
+  geom_line(aes(colour = as.factor(H))) + 
+  facet_grid(rows = vars(V), cols = vars(L), labeller = names) +
+  labs(title = "PAR", x = "FT height", y = "Absorbed PAR", colour = "Height")
+
+# create data frame with absPAR as variable and all the different variables 
+names <- as_labeller(
+  c(`0` = "V 0", `0.1` = "V 0.1", `0.25` = "V 0.25", `0.5` = "V 0.5", `0.75` = "V 0.75", 
+    `0.466` = "LAI 0.467", `1.488`= "LAI 1.349", `2.916`= "LAI 2.917", `4.402`= "LAI 4.485", `5.485`= "LAI 5.395")
+)
+
+mn_outputs_A %>% 
+  select(F, absPAR, H, V, L) %>%
+  ggplot(aes(F, absPAR)) + 
+  geom_point(aes(colour = as.factor(H))) + 
+  geom_line(aes(colour = as.factor(H))) + 
+  facet_grid(rows = vars(V), cols = vars(L), labeller = names) +
+  labs(title = "PAR", x = "FT height", y = "Absorbed PAR", colour = "Height")
 
 
 
