44b4b981c5b25627ad57127c50ad91f9ca76832b
lilydun <l.dun@student.unsw.edu.au>
2021-09-09 10:06:52 +1000

    fixed functions: organising_results,  deep_leaf_distribtuion and deep_crown

--- models/Function_M.R
+++ models/Function_M.R
@@ -39,59 +39,7 @@ model_simp_f <- function(data) {
     as_tibble()
 }
 
-model_simp_nf <- function(data) {
-  df <- data %>% 
-    mutate(t_h = htcrown + httrunk) %>% 
-    arrange(desc(t_h)) %>% 
-    group_by(t_h, focal) %>% 
-    summarise(larea = sum(larea)) %>% 
-    ungroup() %>% 
-    arrange(desc(t_h)) %>% 
-    mutate(
-      lai = cumsum(larea)/10204, 
-      light_ft = exp(-0.5*lai),
-      lai_ppa = floor(lai),
-      light_ppa = exp(-0.5*lai_ppa)) %>% 
-    filter(focal == "FALSE")
-}
-
-plotting <- function(d_nf, d_f) {
-  df_nf <- 
-    bind_rows(d_nf, d_nf %>% mutate(t_h = lead(t_h), new = "new")) %>% 
-    arrange(desc(t_h)) %>%
-    replace_na(list(new = "og"))
-  df_f <- 
-    bind_rows(d_f, d_f %>% mutate(t_h = lead(t_h), new = "new")) %>% 
-    arrange(desc(t_h)) %>%
-    replace_na(list(new = "og")) %>% 
-    filter(focal == "TRUE", new == "og")
-  ggplot(data = df_nf, aes(t_h, light_ft)) +
-    geom_line(colour='green') +
-    geom_line(aes(t_h, light_ppa), colour='blue') +
-    geom_point(data = df_f, aes(t_h, light_ft), colour='red') +
-    geom_point(data = df_f, aes(t_h, light_ppa), colour='orange')
-}
-
-PAR_calculator_ft <- function(data, indi_la = 47.62) {
-  met <- readmet(filename = "met.dat", nlines = -1)
-  la = indi_la
-  L_ft = data %>% 
-    filter(focal == "TRUE") %>% 
-    pull(lai) 
-  PAR_calculator <- 
-    tibble(
-      PAR_inst = met$PAR, 
-      time = met$TIME) %>% 
-    mutate(
-      delta_t = c(diff(time[1:2]), diff(time)),
-      absPAR_inst = PAR_inst*exp(-0.5*L_ft),#mu PAR m-2 s-1
-      W = absPAR_inst/2.02, #W PAR m-2 eqn 38 from paper 
-      MJ = W*0.0036 #MJ PAR m-2 h-1 eqn 12 from paper
-    ) %>% 
-    summarise(absPAR = (sum(MJ*delta_t)*la)) #MJ PAR tree-1 day-1 
-}
-
-new_cal <- function(data) {
+PAR_calculator <- function(data, indi_la = 47.62) {
   # conatnt to converts ymol/s to M J / h
   UMOLperStoMJperH <-
     3600 /  # s / hr
@@ -110,50 +58,8 @@ new_cal <- function(data) {
       MJ_per_H = PAR  * UMOLperStoMJperH  # MJ
     )
   # integrate over the day using trapezoidal integration
-  pracma::trapz(met$time, met$MJ_per_H)*47.62 %>% as_tibble() %>% 
+  pracma::trapz(met$time, met$MJ_per_H) * indi_la %>% as_tibble() %>% 
     rename(absPAR = value)
-}
-
-# conatnt to converts ymol/s to M J / h
-UMOLperStoMJperH <-
-  3600 /  # s / hr
-  4.57 /  # umol quanta / J
-  10^6    # J / MJ
-#getting the LAI above the focal tree
-data = results_f[["H15_V0.1_L4.402_F1.75_S3"]]
-L_ft = data %>% 
-  filter(focal == "TRUE") %>% 
-  pull(lai) 
-# load met and convert PAR to J/hr
-met <- 
-  readmet(filename = "met.dat", nlines = -1) %>% 
-  as_tibble() %>% 
-  select(time = TIME, PAR) %>%
-  mutate(      
-    PAR = 0.77 * PAR * exp(-0.77*L_ft),
-    MJ_per_H = PAR  * UMOLperStoMJperH  # MJ
-  )
-
-# integrate over the day using trapezoidal integration
-pracma::trapz(met$time, met$MJ_per_H)*47.62
-
-PAR_calculator_ppa <- function(data,indi_la = 47.62) {
-  met <- readmet(filename = "met.dat", nlines = -1)
-  la = indi_la
-  L_ft = data %>% 
-    filter(focal == "TRUE") %>% 
-    pull(lai_ppa) 
-  PAR_calculator <- 
-    tibble(
-      PAR_inst = met$PAR, 
-      time = met$TIME) %>% 
-    mutate(
-      delta_t = c(diff(time[1:2]), diff(time)),
-      absPAR_inst = PAR_inst*exp(-0.5*L_ft),#mu PAR m-2 s-1
-      W = absPAR_inst/2.02, #W PAR m-2 eqn 38 from paper 
-      MJ = W*0.0036 #MJ PAR m-2 h-1 eqn 12 from paper
-    ) %>% 
-    summarise(absPAR = (sum(MJ*delta_t)*la))
 }
 
 combining_results_ft <- function(sim_name) {
@@ -175,31 +81,19 @@ combining_results_ppa <- function(sim_name) {
 }
 
 organising_results <- function(data) {
-  df <- as.data.frame(matrix(unlist(data), nrow=length(unlist(data[1])))) 
-  row.names(df) <- c("H", "V", "L", "F", "S", "path", "name", "absPAR")
-  df <- t(df)
-  row.names(df) <- 1:nrow(combinations)
-  df <- df %>% as_tibble() %>% 
-    select(H, V, L, F, S, path, absPAR) %>% 
+  df <- plyr::ldply(data, data.frame) %>% 
+    select(H, V, L, F, S, name, absPAR)  %>% 
     mutate(
-      name = path %>% basename() %>% gsub("_S[1-3]", "",., perl = TRUE)
+      name = name %>% gsub("_S[1-3]", "",., perl = TRUE)
     )
-  df <-  df %>% 
-    transform(absPAR = as.numeric(absPAR), F = as.numeric(F), 
-                  H = as.numeric(H), V = as.numeric(V),
-                  L = as.numeric(L), S = as.numeric(S))
   df <- df %>% 
     group_by(H, V, L, F, name) %>% 
     summarise_at(vars(absPAR), mean)
 }
 
-deep_leaf_distribtuion <- function(httrunk) {
+deep_leaf_distribtuion <- function(httrunk, htcrown = 6.36, radx = 2.54, rady = 2.54, 
+                                   larea = 47.62, n_slices = 50) {
   # adaption from Maeswrap, https://github.com/RemkoDuursma/Maeswrap/blob/master/R/coord3dshape.R
-  htcrown = 6.36
-  radx = 2.54
-  rady = 2.54
-  larea = 47.62
-  n_slices <- 50
   z <- seq(0, 1, length.out = n_slices)
   zabs <- z*htcrown
   distfun <- sqrt(1 - ((z-1/2)^2)/((1/2)^2))
@@ -213,32 +107,33 @@ deep_leaf_distribtuion <- function(httrunk) {
     larea = larea*slice_volume_frac
     )
 }
-
+  
 deep_crown <- function(d) {
-  trying <- d$httrunk[1:nrow(d)] %>% 
+  deep_crown_distribution <- 
+    d$httrunk[1:nrow(d)] %>% 
     purrr::map(deep_leaf_distribtuion) 
-  data <- plyr::ldply(trying, data.frame)
-  df <- as_tibble(rep(1:length(trying), times = 50)) %>% 
-    arrange(value)
-  data <- data %>% cbind(df) 
-  data <- data %>% 
+  df <- tibble(
+    tree_num = rep(1:length(deep_crown_distribution), times = 50)) %>% 
+    arrange(tree_num)
+  data <- plyr::ldply(deep_crown_distribution, data.frame) %>% 
+    cbind(df) %>% 
     arrange(desc(h)) %>% 
     mutate(la = cumsum(larea)/10204)
   if(nrow(data) == 100*50) {
     data <- data %>% 
-      filter(value == 55)
+      filter(tree_num == 55)
   } else if(nrow(data) == 289*50) {
     data <- data %>% 
-      filter(value == 145)
+      filter(tree_num == 145)
   } else if(nrow(data) == 625*50) {
     data <- data %>% 
-      filter(value == 313)
+      filter(tree_num == 313)
   } else if(nrow(data) == 961*50) {
     data <- data %>% 
-      filter(value == 481)
+      filter(tree_num == 481)
   } else if(nrow(data) == 1156*50) {
     data <- data %>% 
-      filter(value == 595)
+      filter(tree_num == 595)
   }
 }
 
