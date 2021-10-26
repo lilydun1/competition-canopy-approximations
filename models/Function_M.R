ac296cbe7141aad796dd03c009507d7a444b09eb
Daniel Falster <daniel.falster@unsw.edu.au>
2021-10-26 11:08:34 +1100

    Expand to all experiments

--- models/Function_M.R
+++ models/Function_M.R
@@ -1,3 +1,94 @@
+load_maespa_results <- function(path, per_leaf_area = TRUE) {
+  results <- read_csv(path) %>%
+    select(H, V, L, F, fla, name, 
+           absPAR_one_s = absPAR,
+           absPAR_two_s = absPAR
+           # NB this is not a mistake! 
+           # maespa doesn't have one stream model, so use two stream for comparison
+           ) %>% 
+    add_column(model = "MAESPA")
+  
+  if(per_leaf_area)
+    results <- results %>%
+      mutate(
+        absPAR_one_s = absPAR_one_s/fla,
+        absPAR_two_s = absPAR_two_s/fla
+    )
+  
+  results
+}
+
+process_experiment <- function(path, H, V, L, F, fla, S, stand = FALSE, max_n =1e4) {
+  
+  # load met forcing
+  met <-  
+    readmet(filename = "met.dat", nlines = -1) %>% 
+    as_tibble() %>% 
+    select(time = TIME, PAR)
+  
+  # Choose function for marking focal trees
+  if(stand) mark_focal_tree <- mark_focal_tree_stand
+  else mark_focal_tree <- mark_focal_tree_single
+  
+  combinations <- 
+    expand_grid(H, V, L, F, fla, S) %>% 
+    mutate(path = sprintf("%s/H%s_V%s_L%s_F%s_fla%s_S%s", path, H, V, L, F, fla, S)) %>% 
+    add_column(name = basename(.$path))  %>% 
+    slice(1:max_n) %>%
+    mutate(trees = map(path, load_trees), 
+           f_trees = map(trees, mark_focal_tree))
+  
+  results <- 
+    combinations %>%
+    mutate(
+      models_lai = map(f_trees, model_lai)
+    ) %>%
+    unnest(models_lai) %>%
+    # calculate light PAR
+    mutate(
+      PAR_one_s = map(lai, PAR_calculator, stream = 1, met = met),
+      PAR_two_s = map(lai, PAR_calculator, stream = 2, met = met),
+      # integrate over the day using trapezoidal integration
+      absPAR_one_s = map_dbl(PAR_one_s, ~pracma::trapz(.x$time, .x$MJ_per_H)),
+      absPAR_two_s = map_dbl(PAR_two_s, ~pracma::trapz(.x$time, .x$MJ_per_H))
+    )
+  results
+}
+
+calculate_averages <- function(results, per_leaf_area = TRUE) {
+
+  # Calculate totals for each plant in each simulation
+  out <-
+    results %>%
+    # sum over leaf segments (needed for deep crown, others only have 1 segment)
+    group_by(H, V, L, F, fla, S, name, model) %>%
+    summarise(
+      t_h = mean(t_h),
+      absPAR_one_s = sum(absPAR_one_s*larea), 
+      absPAR_two_s = sum(absPAR_two_s*larea),
+      larea = sum(larea)
+    ) %>%
+    ungroup() 
+  
+  if(per_leaf_area) {
+    out <- out %>%
+      mutate(
+        absPAR_one_s = absPAR_one_s/larea, 
+        absPAR_two_s = absPAR_two_s/larea
+      )
+  }
+  
+  out %>%
+    # average over replicate simulations S==1, S==2...
+    mutate(
+      name = name %>% gsub("_S[1-3]", "",., perl = TRUE)
+    ) %>% 
+    group_by(H, V, L, F, fla, name, model) %>% 
+    summarise_at(vars(absPAR_one_s, absPAR_two_s), mean) %>%
+    ungroup()
+}
+
+
 load_trees <- function(path) {
   trees <- parseFile(file.path(path,"trees.dat"))
   data <- tibble(tree_num = seq_len(length(trees$indivradx$values)),
@@ -9,7 +100,7 @@ load_trees <- function(path) {
     filter(x > 27.999, x < 168.001, y > 27.999, y < 168.001)
 }
 
-mark_focal_tree <- function(data) {
+mark_focal_tree_single <- function(data) {
     if(nrow(data) == 100) {
       data <- data %>% add_column(focal = ifelse(data$x == 105.54 & data$y == 90.46, TRUE, FALSE))
     } else if(nrow(data) == 324) {
@@ -19,6 +110,19 @@ mark_focal_tree <- function(data) {
     } else if(nrow(data) != 100 & nrow(data) != 324 & nrow(data) != 676) {
       data <- data %>% add_column(focal = ifelse(data$x == 98 & data$y == 98, TRUE, FALSE))
     }
+  data
+}
+
+mark_focal_tree_stand <- function(data) {
+  if(nrow(data) != 961){
+    data <- data %>% add_column(focal = 
+                                  ifelse(data$x > 75.37 & data$x < 120.63 & data$y > 75.37 & data$y < 120.63, 
+                                         TRUE, FALSE))
+  } else if(nrow(data) == 961) {
+    data <- data %>% add_column(focal = 
+                                  ifelse(data$x > 75.72 & data$x < 115.83  & data$y > 75.72 & data$y < 115.83, 
+                                         TRUE, FALSE))
+  }
   data
 }
 
@@ -108,147 +212,5 @@ deep_crown_set_up <- function(d) {
     select(t_h, tree_num, h, focal, larea = larea_slice, lai, model)
 }
 
-#stand functions
-focal_tree_stand <- function(data) {
-  if(nrow(data) != 961){
-    data <- data %>% add_column(focal = 
-                   ifelse(data$x > 75.37 & data$x < 120.63 & data$y > 75.37 & data$y < 120.63, 
-                          TRUE, FALSE))
-  } else if(nrow(data) == 961) {
-      data <- data %>% add_column(focal = 
-                     ifelse(data$x > 75.72 & data$x < 115.83  & data$y > 75.72 & data$y < 115.83, 
-                            TRUE, FALSE))
-  }
-}
-
-met_f <- function(L_ft) {
-  met <- 
-    readmet(filename = "met.dat", nlines = -1) %>% 
-    as_tibble() %>% 
-    select(time = TIME, PAR) %>%
-    mutate(
-      PAR_one_s = 0.77 * PAR * exp(-0.77*L_ft), #one stream
-      PAR_two_s = 0.77*PAR*(exp(-0.77*L_ft) + (1- exp(-0.77*L_ft))*exp(-0.35*L_ft)), #two stream 
-      MJ_per_H_one_s = PAR_one_s * UMOLperStoMJperH, 
-      MJ_per_H_two_s = PAR_two_s * UMOLperStoMJperH  # MJ
-    )
-}
-
-PAR_calculator_ft_stand <- function(data, indi_la = 1) {
-  # constant to converts ymol/s to M J / h
-  UMOLperStoMJperH <-
-    3600 /  # s / hr
-    4.57 /  # umol quanta / J
-    10^6    # J / MJ
-  L_ft <- data %>% 
-    pull(lai) 
-  # load met and convert PAR to J/hr
-  trying <- L_ft %>% 
-    map(met_f)
-  # integrate over the day using trapezoidal integration
-  for(i in 1:length(trying)){
-    trying[[i]] <- tibble(absPAR_one_s = pracma::trapz(trying[[i]]$time, trying[[i]]$MJ_per_H_one_s) * indi_la, 
-                          absPAR_two_s = pracma::trapz(trying[[i]]$time, trying[[i]]$MJ_per_H_two_s) * indi_la)
-  }
-  df <- plyr::ldply(trying, data.frame) %>% 
-    mutate(absPAR_one_s = absPAR_one_s*0.1, 
-           absPAR_two_s = absPAR_two_s*0.1) %>% 
-    summarise(absPAR_one_s = sum(absPAR_one_s), 
-              absPAR_two_s = sum(absPAR_two_s))
-}
-
-PAR_calculator_ppa_stand <- function(data, indi_la = 1) {
-  # constant to converts ymol/s to M J / h
-  UMOLperStoMJperH <-
-    3600 /  # s / hr
-    4.57 /  # umol quanta / J
-    10^6    # J / MJ
-  L_ft <- data %>% 
-    pull(lai_ppa) 
-  # load met and convert PAR to J/hr
-  trying <- L_ft %>% 
-    map(met_f)
-  # integrate over the day using trapezoidal integration
-  for(i in 1:length(trying)){
-    trying[[i]] <- tibble(absPAR_one_s = pracma::trapz(trying[[i]]$time, trying[[i]]$MJ_per_H_one_s) * indi_la, 
-                          absPAR_two_s = pracma::trapz(trying[[i]]$time, trying[[i]]$MJ_per_H_two_s) * indi_la)
-  }
-  df <- plyr::ldply(trying, data.frame) %>%    
-    mutate(absPAR_one_s = absPAR_one_s*0.1, 
-           absPAR_two_s = absPAR_two_s*0.1) %>% 
-    summarise(absPAR_one_s = sum(absPAR_one_s), 
-              absPAR_two_s = sum(absPAR_two_s))
-}
-
-deep_crown_set_up_stand <- function(d) {
-  LAI0.5_focal = c(34:37, 44:47, 54:57, 64:67)
-  LAI1.5_focal = c(115:120, 133:138 , 151:156 , 170:175 , 187:192 , 205:210)
-  LAI3_focal = c(244:251, 270:277, 296:303, 322:329, 348:355, 374:381, 
-                 400:407, 426:433)
-  LAI4.5_focal = c(321:330, 352:361, 383:392, 414:423, 445:454, 476:485, 
-                   507:516, 538:547, 569:578, 600:609)
-  LAI5.5_focal = c(433:443, 468:478, 503:513, 538:548, 573:583, 608:618, 
-                   643:653, 678:688, 713:723, 748:758, 783:793)
-  deep_crown_distribution <- 
-    d$httrunk[1:nrow(d)] %>% 
-    purrr::map(deep_leaf_distribtuion) 
-  df <- tibble(
-    tree_num = rep(1:length(deep_crown_distribution), times = 50)) %>% 
-    arrange(tree_num)
-  data <- plyr::ldply(deep_crown_distribution, data.frame) %>% 
-    cbind(df) %>% 
-    arrange(desc(h)) %>% 
-    mutate(la = cumsum(larea)/6123)
-  if(nrow(data) == 100*50) {
-    data <- data %>% 
-      filter(tree_num %in% LAI0.5_focal)
-  } else if(nrow(data) == 324*50) {
-    data <- data %>% 
-      filter(tree_num %in% LAI1.5_focal)
-  } else if(nrow(data) == 676*50) {
-    data <- data %>% 
-      filter(tree_num %in% LAI3_focal)
-  } else if(nrow(data) == 961*50) {
-    data <- data %>% 
-      filter(tree_num %in% LAI4.5_focal)
-  } else if(nrow(data) == 1225*50) {
-    data <- data %>% 
-      filter(tree_num %in% LAI5.5_focal) 
-  }
-}
-
-PAR_calculator_DC_stand <- function(la) {
-  UMOLperStoMJperH <-
-    3600 /  # s / hr
-    4.57 /  # umol quanta / J
-    10^6    # J / MJ
-  met <- readmet(filename = "met.dat", nlines = -1) %>% 
-    as_tibble() %>% 
-    select(time = TIME, PAR) %>% 
-    mutate(  
-      PAR_one_s = 0.77 * PAR * exp(-0.77*la),
-      PAR_two_s = 0.77* PAR*(exp(-0.77*la) + (1- exp(-0.77*la))*exp(-0.35*la)), 
-      MJ_per_H_one_s = PAR_one_s * UMOLperStoMJperH, 
-      MJ_per_H_two_s = PAR_two_s * UMOLperStoMJperH )
-  tibble(absPAR_one_s = pracma::trapz(met$time, met$MJ_per_H_one_s) * 0.1, 
-         absPAR_two_s = pracma::trapz(met$time, met$MJ_per_H_two_s) * 0.1) 
-}
-
-applying_DC_stand <- function(data) {
-  data$la[1:nrow(data)] %>% 
-    map_df(PAR_calculator_DC_stand)
-}
-
-summarise_DC_stand <- function(data) {
-  df <- tibble(tree = rep(1:(nrow(data)/50), times = 50)) %>% 
-    arrange(tree)
-  d <- cbind(data, df) 
-  da <- d %>% group_by(tree) %>% 
-    summarise(absPAR_one_s = mean(absPAR_one_s), 
-              absPAR_two_s = mean(absPAR_two_s)) %>% 
-    select(absPAR_one_s, absPAR_two_s) %>% 
-    summarise(absPAR_one_s = sum(absPAR_one_s), 
-              absPAR_two_s = sum(absPAR_two_s))
-}
 
 
